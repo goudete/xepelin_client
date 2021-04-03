b7e45bf4456f030edbd91cf295e951e1a69358bd
goudete <goudetenrique@gmail.com>
2021-04-03 12:30:05 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [domains, setDomains] = useState([]);
+
+  // Get all existing domains
+  useEffect(() => {
+    axios.get('/domain')
+      .then((response) => {
+        console.log(response);
+        setDomains(response)
+      });
+  });
+
+  const newShortUrl = (newUrl) => {
+    axios.post('/shorten', {
+      url: newUrl,
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{paddingBottom: "50px"}}>
+          Domains Registered:
+          {/* display domains here */}
+        </div>
+
+        <form>
+          <label>
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Add New short url" />
+        </form>
       </header>
     </div>
   );
