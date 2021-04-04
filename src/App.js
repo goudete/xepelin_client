b5177898f8e9f65a037208284adcac32b3b0f47c
goudete <goudetenrique@gmail.com>
2021-04-04 14:33:59 -0500

    progress

--- src/App.js
+++ src/App.js
@@ -6,39 +6,71 @@ import axios from 'axios';
 function App() {
 
   const [domains, setDomains] = useState([]);
+  const [urls, setUrls] = useState([]);
+  const [formValue, setValue] = useState();
 
   // Get all existing domains
   useEffect(() => {
+    console.log('in use effect')
     axios.get('/domain')
       .then((response) => {
-        console.log(response);
-        setDomains(response)
+        console.log(response.data.domains);
+        let keys = Object.keys(response.data.domains);
+        setDomains(keys);
+        setUrls(response.data.domains);
       });
   });
 
-  const newShortUrl = (newUrl) => {
-    axios.post('/shorten', {
-      url: newUrl,
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  const newShortUrl = () => {
+    if (formValue.substring(0,4) == "http") {
+      axios.post('/shorten', {
+        url: formValue,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    } else {
+      let h = 'http://'
+      let res = h.concat(formValue);
+      console.log('url:', res)
+      axios.post('/shorten', {
+        url: res,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+    
   }
+  const expandDomain = (domainName) => {
+    let urlsArr = urls.domainName;
+    urlsArr.map((u) => {
+      <h5>{u}</h5>
+    })
+  }
+  const domainsArray = domains.map((m) => {
+    <h2 key={m} onClick={expandDomain(m)}>{m}</h2>
+  })
 
   return (
     <div className="App">
+
       <header className="App-header">
         <div style={{paddingBottom: "50px"}}>
           Domains Registered:
-          {/* display domains here */}
+          {domainsArray}
+          {expandDomain}
         </div>
 
-        <form>
+        <form onSubmit={newShortUrl}>
           <label>
-            <input type="text" name="name" />
+            <input type="text" value={formValue} onChange={e => setValue(e.target.value)} />
           </label>
           <input type="submit" value="Add New short url" />
         </form>
